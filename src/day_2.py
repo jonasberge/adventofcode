88150809ad55cd62e6ca38629d9c4929ae7de6b8
vonas <24623262+vonas@users.noreply.github.com>
2020-12-02 16:09:44 +0100

    Extract base class and parsing function

--- src/day_2.py
+++ src/day_2.py
@@ -7,23 +7,36 @@ input = read_lines(2)
 
 
 class Rule:
-  def __init__(self, letter, min, max):
+  def __init__(self, n1, n2, letter):
+    self.n1 = n1
+    self.n2 = n2
     self.letter = letter
+
+
+class OldRule(Rule):
+  def __init__(self, min, max, letter):
+    super().__init__(min, max, letter)
     self.range = range(min, max + 1)
 
   def is_valid(self, password):
     return password.count(self.letter) in self.range
 
 
-def count_valid():
+def parse_line(line):
+  match = re.match(r"^(\d+)-(\d+)\s(\w):\s(\w*)$", line)
+  n1, n2, letter, password = match.groups()
+  n1, n2 = int(n1), int(n2)
+
+  return n1, n2, letter, password
+
+
+def count_valid(RuleType):
   valid_count = 0
 
   for line in input:
-    match = re.match(r"^(\d+)-(\d+)\s(\w):\s(\w*)$", line)
-    lo, hi, letter, password = match.groups()
-    lo, hi = int(lo), int(hi)
+    n1, n2, letter, password = parse_line(line)
 
-    rule = Rule(letter, lo, hi)
+    rule = RuleType(n1, n2, letter)
     if rule.is_valid(password):
       valid_count += 1
 
@@ -31,4 +44,4 @@ def count_valid():
 
 
 def solve():
-  return count_valid()
+  return count_valid(OldRule)
