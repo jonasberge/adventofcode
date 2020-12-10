b56c1baba520bdfb1e6fef027c02036af3668aa8
vonas <24623262+vonas@users.noreply.github.com>
2020-12-10 20:08:37 +0100

    Add test cases for day 10 - part 2

--- test/test_day_10.py
+++ test/test_day_10.py
@@ -14,7 +14,14 @@ class part_1:
     assert problem.solve()[0] == 2400
 
 
-@skip
 class part_2:
+  @use_example(1)
+  def test_example_1(self, problem):
+    assert problem.solve()[1] == 8
+
+  @use_example(2)
+  def test_example_2(self, problem):
+    assert problem.solve()[1] == 19208
+
   def test_solution(self, problem):
-    assert problem.solve()[1] == 0000
+    assert problem.solve()[1] == 338510590509056
