870c69b36ae0a8f81df8c1ab59516e709ad04c50
vonas <24623262+vonas@users.noreply.github.com>
2020-12-12 00:24:46 +0100

    Add option to calculate a single part

--- solve.py
+++ solve.py
@@ -3,15 +3,21 @@ from sys import argv, exit
 
 
 if __name__ == '__main__':
-  if len(argv) != 2: exit(1)
+  if len(argv) not in (2, 3): exit(1)
 
   day = argv[1]
+  part = int(argv[2]) if len(argv) == 3 else None
 
   try: module = import_module('src.day_{}'.format(day))
   except Exception as e:
     raise e
 
-  print((
-    module.solve_1(),
-    module.solve_2()
-  ))
+  if part:
+    if part not in (1, 2):
+      raise Exception('Part number must be either 1 or 2')
+    solve = module.solve_1 if part == 1 else module.solve_2
+    print(solve())
+    exit()
+
+  print(module.solve_1(), flush=True)
+  print(module.solve_2())
