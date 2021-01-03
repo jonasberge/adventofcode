6a36275b4597f6a95dc5995c542bb6fc567d1ca0
vonas <24623262+vonas@users.noreply.github.com>
2021-01-03 16:39:24 +0100

    Solve day 19 - part 2

--- src/day_19.py
+++ src/day_19.py
@@ -32,9 +32,9 @@ def parse_rules(lines):
   return rules
 
 
-def create_regex(rules):
-  first = FRULE.format(str(FIRST_RULE))
-  result = r"^(" + first + r")$"
+def create_regex(rules, start=FIRST_RULE):
+  first = FRULE.format(str(start))
+  result = "({})".format(first)
 
   todo = deque()
   todo.append(first)
@@ -50,16 +50,71 @@ def create_regex(rules):
   return re.compile(result)
 
 
-def count_full_matches():
+def count_matches():
   it = iter(input)
   rules = parse_rules(it)
   regex = create_regex(rules)
+  regex = re.compile(r"^" + regex.pattern + r"$")
   return sum(bool(regex.match(line)) for line in it)
 
 
-def part_2():
-  return None
+def try_match_start(regex, string):
+  match = regex.match(string)
+  if not match:
+    return False, string
+
+  _, end = match.span()
+  return True, string[end:]
 
 
-solve_1 = lambda: count_full_matches()
-solve_2 = lambda: part_2()
+def try_match_end(regex, string):
+  match = re.match(r"(.*)" + regex.pattern + r"$", string)
+  if not match:
+    return False, string
+
+  prefix = match.groups()[0]
+  return True, prefix
+
+
+def match_special(rules, line):
+  p42 = create_regex(rules, 42).pattern
+  p31 = create_regex(rules, 31).pattern
+
+  r42, r31 = re.compile(p42), re.compile(p31)
+  r_start = re.compile(p42 + r"+")
+
+  s = line
+
+  # start matching from the end.
+  # count the number of matches of rule #31 at the end,
+  # then apply rule #42 as often as that to the end as well.
+  # finally match rule #8 at least once from the start.
+  # if all attempts matched and the remaining string is empty,
+  # then the string matches the modified rules.
+
+  c1 = 0
+  while True:
+    matched, s = try_match_end(r31, s)
+    if matched: c1 += 1
+    else: break
+
+  if c1 < 1:
+    return False
+
+  for _ in range(c1):
+    matched, s = try_match_end(r42, s)
+    if not matched:
+      return False
+
+  matched, s = try_match_start(r_start, s)
+  return matched and len(s) == 0
+
+
+def count_modified_matches():
+  it = iter(input)
+  rules = parse_rules(it)
+  return sum(match_special(rules, line) for line in it)
+
+
+solve_1 = lambda: count_matches()
+solve_2 = lambda: count_modified_matches()
--- test/test_day_19.py
+++ test/test_day_19.py
@@ -7,14 +7,21 @@ class part_1:
     assert problem.solve_1() == 8
 
   @use_example(2)
-  def test_example_1(self, problem):
+  def test_example_2(self, problem):
     assert problem.solve_1() == 2
+
+  @use_example(3)
+  def test_example_3(self, problem):
+    assert problem.solve_1() == 3
 
   def test_solution(self, problem):
     assert problem.solve_1() == 165
 
 
-@skip
 class part_2:
+  @use_example(3)
+  def test_example_3(self, problem):
+    assert problem.solve_2() == 12
+
   def test_solution(self, problem):
-    assert problem.solve_2() == 0000
+    assert problem.solve_2() == 274
